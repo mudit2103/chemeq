cc0d619c7c8e137009371760ccbc679285c6cb65
Mudit <mudit2103@gmail.com>
2017-01-06 12:08:14 +0400

    working without chemical structres

--- bin/app.py
+++ bin/app.py
@@ -16,37 +16,43 @@ class Index(object):
 
     def POST(self):
         form = web.input(equation="Hello")
-        # compound1 = cs.search(form.equation)
-        # compound2 = cs.search(form.name)
-
-        two_halves = form.equation.split('->')
-        string = two_halves
-        left = two_halves[0]
-        right = two_halves[1]
-
-        left = left.split('+')
-        right = right.split('+')
-
-        left = [cs.search(x)[0].molecular_formula for x in left]
-        right = [cs.search(x)[0].molecular_formula for x in right]
-
-
-        left = ' + '.join(left)
-        right = ' + '.join(right)
-        string = left + r" \rightarrow " + right
+        # also_left = [cs.search(x)[0].image_url.encode('ascii','ignore') for x in left]
+        expressions = form.equation.split('->') # expressions is a list of expressions like (compound1 + compound2 + ...)
+        compounds_expressions = [x.split('+') for x in expressions] # list of lists
+        molecular_formulas = [[cs.search(compound)[-1].molecular_formula for compound in expression] for expression in compounds_expressions]
+        chemical_expressions = [' + '.join(x) for x in molecular_formulas]
+        chem_eqn = r" \rightarrow ".join(chemical_expressions)
 
 
 
+        return render.index(equation = chem_eqn)
 
 
+def is_organic(compound):
+    SMILES_string = compound.smiles
 
+    elements_with_c = ['Cd', 'Ca', 'Cf', 'Ce', 'Cs', 'Cl', 'Cr', 'Co', 'Cu', 'Cm']
 
+    for x in elements_with_c:
+        SMILES_string = SMILES_string.replace(x, "")
 
+    if 'C' in SMILES_string or 'c' in SMILES_string:
+        return True
+    else:
+        return False
 
-
-
-        greeting = "%s" % (string)
-        return render.index(equation = greeting)
 
 if __name__ == "__main__":
+    # benzene = cs.search("benzene")[-1]
+    # toluene = cs.search("toluene")[-1]
+    # alcl3 = cs.search("alcl3")[-1]
+    # h2 = cs.search("h2")[-1]
+
+    # print("benzene", is_organic(benzene))
+    # print("toluene", is_organic(toluene))
+    # print("alcl3", is_organic(alcl3))
+    # print("h2", is_organic(h2))
+    # print("chlorobenzene", is_organic(cs.search("chlorobenzene")[-1]))
+
+
     app.run()
